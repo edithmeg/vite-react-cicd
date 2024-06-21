dcb09b21701d78d78e96576b37c468c97ccea9dd
Edith Mejia <edith.mejia@accenture.com>
2024-06-21 15:59:53 -0600

    chore: app updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { addingNumbers } from './utils'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
@@ -19,15 +19,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+       Deployed by Edith for MFEE 
+       
       </div>
       <p className="read-the-docs">
-        {addingNumbers(5+1)}
+        
       </p>
     </>
   )
